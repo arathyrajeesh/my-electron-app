8091ffe3d86d867537b56fa5b91bd7244c3f334a
arathyrajeesh <arathyrajeesh2@gmail.com>
2026-03-14 15:42:17 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+
+    const win = new BrowserWindow({
+        width: 300,
+        height: 400,
+        webPreferences: {
+        nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
